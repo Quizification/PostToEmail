93948d003362752b252a08c04708f13c25629901
Robin Schleser <robinschleser0608@gmail.com>
2022-03-16 13:55:49 +0100

    Mailer

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const nodemailer = require('nodemailer');
 const express = require('express');
+const bodyParser = require('body-parser');
+require("dotenv").config();
 
 const server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 4848;
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
-  }
+  },
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  requireTLS: true
 })
 
 server.get('/', (req, res) => {
@@ -23,11 +33,12 @@ server.post('/', (req, res) => {
     subject: 'New Inquiry',
     text: `${req.body.name}
     ${req.body.email}
-    ${req.body.message}`,
+    ${req.body.message} & ${req.body.data}`,
     html: `<h1>New Inquiry</h1>
     <p>${req.body.name}</p>
     <p>${req.body.email}</p>
-    <p>${req.body.message}</p>`,
+    <p>${req.body.data}</p>
+    <pre>${req.body.message}</pre>`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -36,7 +47,12 @@ server.post('/', (req, res) => {
       res.send(error);
     } else {
       console.log('Email sent: ' + info.response);
-      res.send(info.response);
+      res.send("Successfully sended");
     }
   });
+
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
